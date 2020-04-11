952db153bdb1bee51ca5d0fd7d1840755dbd732e
Sergey Mashkov <cy6erGn0m@gmail.com>
2020-04-11 13:47:08 +0300

    Add web repositories

--- src/main/java/ru/levelup/db/UsersRepository.java
+++ src/main/java/ru/levelup/db/UsersRepository.java
@@ -2,6 +2,8 @@ package ru.levelup.db;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import ru.levelup.model.User;
 import ru.levelup.model.UserStatus;
@@ -9,8 +11,11 @@ import ru.levelup.model.UserStatus;
 import java.util.List;
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "users-api",
+        itemResourceRel = "users-api",
+        path = "users-api")
 public interface UsersRepository extends CrudRepository<User, Integer> {
-    User findByLogin(String login);
+    User findByLogin(@Param("loginName") String login);
     User findByLoginAndPassword(String login, String password);
 
     List<User> findByStatusOrderByLogin(UserStatus status);
--- src/main/java/ru/levelup/web/WebConfiguration.java
+++ src/main/java/ru/levelup/web/WebConfiguration.java
@@ -1,6 +1,8 @@
 package ru.levelup.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -9,6 +11,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
+@Import(RepositoryRestMvcConfiguration.class)
 public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
