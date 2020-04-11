04f4351034ded373a538a28d665db0192e9f1348
Sergey Mashkov <cy6erGn0m@gmail.com>
2020-04-11 14:08:18 +0300

    Add paging and sorting

--- src/main/java/ru/levelup/db/UsersRepository.java
+++ src/main/java/ru/levelup/db/UsersRepository.java
@@ -1,7 +1,9 @@
 package ru.levelup.db;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
@@ -14,13 +16,13 @@ import java.util.List;
 @RepositoryRestResource(collectionResourceRel = "users-api",
         itemResourceRel = "users-api",
         path = "users-api")
-public interface UsersRepository extends CrudRepository<User, Integer> {
+public interface UsersRepository extends PagingAndSortingRepository<User, Integer> {
     User findByLogin(@Param("loginName") String login);
     User findByLoginAndPassword(String login, String password);
 
-    List<User> findByStatusOrderByLogin(UserStatus status);
+    Page<User> findByStatusOrderByLogin(UserStatus status, Pageable page);
 
-    List<User> findByBonusScoreGreaterThan(int bonusScore);
+    Page<User> findByBonusScoreGreaterThan(int bonusScore, Pageable page);
 
     @Query("select u from User u where u.bonusScore > :bonusScore")
     List<User> findUsersScoreTooBig(int bonusScore);
--- src/main/java/ru/levelup/web/UsersRestController.java
+++ src/main/java/ru/levelup/web/UsersRestController.java
@@ -1,13 +1,20 @@
 package ru.levelup.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.levelup.db.UsersRepository;
 import ru.levelup.model.User;
+import ru.levelup.model.UserStatus;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class UsersRestController {
@@ -21,6 +28,22 @@ public class UsersRestController {
             allUsers.add(user);
         }
         return allUsers;
+    }
+
+    @PostMapping("/api/ban-user")
+    public String badUser(@RequestParam String login) {
+        User user = repository.findByLogin(login);
+        user.setStatus(UserStatus.BANNED);
+        repository.save(user);
+        return "OK";
+    }
+
+    @GetMapping("/api/banned-users")
+    public List<User> findBannedUsers(@RequestParam int pageNumber) {
+        Page<User> page = repository.findByStatusOrderByLogin(UserStatus.BANNED,
+                PageRequest.of(pageNumber - 1, 10));
+
+        return page.get().collect(Collectors.toList());
     }
 
     @GetMapping("/api/logins")
